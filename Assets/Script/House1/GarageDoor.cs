1e2c0553d2d75635d084678b345a097f140c8ca5
luantran2k <75030293+luantran2k@users.noreply.github.com>
2022-01-03 17:51:42 +0700

    create house 1

---
+++ Assets/Script/House1/GarageDoor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarageDoor : MonoBehaviour
+{
+    Transform door;
+    int doorStatus, speed = 2;
+    private void Start()
+    {
+        door = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if(doorStatus == 1)
+        {
+            OpenGarageDoor();
+        }
+        else if(doorStatus == 2)
+        {
+            CloseGarageDoor();
+        }
+    }
+
+    public void GarageDoorControl()
+    {
+        if(door.position.y <= 0)
+        {
+            doorStatus = 1;
+        }
+        else
+        {
+            doorStatus = 2;
+        }
+    }
+
+    public void OpenGarageDoor()
+    {
+        if(door.position.y < 4)
+        {
+            door.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        else
+        {
+            doorStatus = 0;
+        }
+        
+    }
+
+    public void CloseGarageDoor()
+    {
+        if (door.position.y > 0)
+        {
+            door.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+        else
+        {
+            doorStatus = 0;
+        }
+    }
+}
