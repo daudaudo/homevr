dda5607fb873ab64d891e47a55baf48348e79653
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2022-01-03 10:56:52 +0700

    add Speech Recogniation

---
+++ Assets/Script/SpeechRecognitionEngine.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+/// <summary>
+/// see here https://lightbuzz.com/speech-recognition-unity/
+/// </summary>
+public class SpeechRecognitionEngine : MonoBehaviour
+{
+    private string[] keywords = new string[] { "move", "stop", "menu", "hide" };
+    public ConfidenceLevel confidence = ConfidenceLevel.Rejected;
+    protected PhraseRecognizer recognizer;
+
+    private void Start()
+    {
+        if (keywords != null)
+        {
+            recognizer = new KeywordRecognizer(keywords, confidence);
+            recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+            recognizer.Start();
+            Debug.Log( recognizer.IsRunning );
+        }
+
+        foreach (var device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
+    }
+
+    void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log("You said: <b>" + args.text + "</b>");
+    }
+
+    private void Update()
+    {
+
+    }
+
+    void OnDestroy()
+    {
+        if (recognizer != null && recognizer.IsRunning)
+        {
+            recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+            recognizer.Stop();
+        }
+    }
+}
