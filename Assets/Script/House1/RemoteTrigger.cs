393f8efd91f1a730fa0c96792e87b944c7938745
luantran2k <75030293+luantran2k@users.noreply.github.com>
2022-01-04 21:18:45 +0700

    update scene

--- Assets/Script/House1/RemoteTrigger.cs
+++ Assets/Script/House1/RemoteTrigger.cs
@@ -7,17 +7,23 @@ public class RemoteTrigger : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
     public GameObject TvCover;
-    bool state;
+    public AudioSource audio;
+    bool state, isSpeak;
     // Start is called before the first frame update
     void Start()
     {
         state = false;
+        isSpeak = false;
     }
     
     public void UseRemote()
     {
+        isSpeak = true;
         if (state == false)
         {
+            if (!isSpeak) {
+                audio.Play();
+            }      
             Destroy(TvCover);
             videoPlayer.Play();
             state = true;
