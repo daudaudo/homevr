739172fb45c2b83625871be19235040ff533eb8c
thanhlieng <thanh.271020@gmail.com>
2022-01-05 20:55:08 +0700

    add player rotate follow cam

---
+++ Assets/Script/PlayerRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera cam;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.rotation = Quaternion.Euler(0,cam.transform.eulerAngles.y,0);
+    }
+}
