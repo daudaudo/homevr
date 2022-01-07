e68901fb0eedaf5a42b55c392aced335ed7a403d
Traan Hai Nam <teenhainam2603@gmail.com>
2022-01-07 12:24:36 +0700

    final

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -9,20 +9,28 @@ public class Menu : MonoBehaviour
     public GameObject menu,menuNext,menuP;
     public Text view;
     public string house;
+    AudioSource pop;
+    private void Start()
+    {
+        pop = GameObject.Find("Pop").GetComponent<AudioSource>();
+    }
     public void Click()
     {
+        pop.Play();
         menu.SetActive(false);
         menuNext.SetActive(true);
     }
     public void Back(){
+        pop.Play();
         menu.SetActive(false);
         menuP.SetActive(true);
     }
     public void selectHouse(){
+        pop.Play();
         view.text = house;
     }
     public void LoadScene(){
-        
+        pop.Play();
         SceneManager.LoadScene(view.text);
     }
 }
